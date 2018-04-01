1ac87c173a3aa204f3da0802f7af4e9e31dbba2e
ehsan MAE <ehsan.m.a.e@gmail.com>
2018-04-01 19:00:35 +0430

    [batch disc]

--- script/simple_model.py
+++ script/simple_model.py
@@ -17,8 +17,8 @@ else:
 dataset_files = ['map1n_allz_rtaapixlw_2048_1.fits', 'map1n_allz_rtaapixlw_2048_2.fits', 'map1n_allz_rtaapixlw_2048_3.fits']
 dp = cs.Data_Provider ([prefix + file_name for file_name in dataset_files], preprocess_mode=2)
 
-batch_size = 16
-image_size = 64
+batch_size = 64
+image_size = 128
 gf_dim = 32
 df_dim = 32
 z_dim = 128
@@ -37,15 +37,22 @@ class NewDCGAN(cs.DCGAN):
 			if reuse:
 				scope.reuse_variables ()
 
-			h0 = lrelu (conv2d (image, self.df_dim, name='d_h0_conv'))
-			h1 = lrelu (self.d_bn1 (conv2d (h0, self.df_dim * 2, name='d_h1_conv')))
-			h2 = lrelu (self.d_bn2 (conv2d (h1, self.df_dim * 4, name='d_h2_conv')))
-			h3 = lrelu (self.d_bn3 (conv2d (h2, self.df_dim * 8, name='d_h3_conv')))
-			h4 = linear (tf.reshape (h3, [1, -1]), self.batch_size * self.df_dim, 'd_h4_lin')
-			h5 = linear (tf.reshape (h4, [1, -1]), self.batch_size, 'd_h5_lin')
-			h6 = linear (tf.reshape (h5, [1, -1]), 1, 'd_h6_lin')
+			h00 = lrelu (conv2d (image, self.df_dim, name='d_h0_conv'))
+			h01 = lrelu (self.d_bn1 (conv2d (h00, self.df_dim * 2, name='d_h01_conv')))
+			h02 = lrelu (self.d_bn2 (conv2d (h01, self.df_dim * 4, name='d_h02_conv')))
+			h03 = lrelu (self.d_bn3 (conv2d (h02, self.df_dim * 8, name='d_h03_conv')))
+			h04 = linear (tf.reshape (h03, [self.batch_size, -1]), 1, 'd_h04_lin') # bs, 1
+			h05 = linear (tf.reshape (h04, [1, -1]), 1, 'd_h05_lin')
 
-			return tf.nn.sigmoid (h6), h6
+			inp = tf.transpose (image, [3, 1, 2, 0])
+			h10 = lrelu (conv2d (inp, 4, name='d_h10_conv'))
+			h11 = lrelu (conv2d (h10, 2, name='d_h11_conv'))
+			h12 = lrelu (conv2d (h11, 1, name='d_h12_conv'))
+			h13 = linear (tf.reshape (h12, [1, -1]), 1, 'd_h13_lin') # 1, 1
+
+			o = linear (tf.concat ([h05, h13], axis=1), 1, 'd_output')
+
+			return tf.nn.sigmoid (o), o
 
 dcgan = NewDCGAN (
 	data_provider=dpp,
@@ -56,7 +63,7 @@ dcgan = NewDCGAN (
 	z_dim=z_dim,
 	checkpoint_dir='checkpoint')
 
-dcgan.train (num_epoch=2000, batch_per_epoch=50, learning_rate=1e-4, D_update_per_batch=1, G_update_per_batch=1, verbose=100, sample_per=10000)
+dcgan.train (num_epoch=4000, batch_per_epoch=50, learning_rate=8e-5, D_update_per_batch=1, G_update_per_batch=1, verbose=100, sample_per=10000)
 sample_z = np.random.normal(size=(200, dcgan.z_dim))
 ress = dcgan.generate(sample_z)
 from PIL import Image
