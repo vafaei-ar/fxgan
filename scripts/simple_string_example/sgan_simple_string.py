85bd6170c1affdbc49ee1be5c2a2222ff238eeb0
vafaei-ar <you@example.com>
2018-05-19 13:57:33 +0430

    simple_string

---
+++ scripts/simple_string_example/sgan_simple_string.py
@@ -0,0 +1,76 @@
+import os
+import sys
+import argparse
+import numpy as np
+import csgan as cs 
+import pylab as plt
+from time import time
+from utils import *
+    
+parser = argparse.ArgumentParser(description='Short sample app')
+parser.add_argument('--show', action="store_true", default=False)
+parser.add_argument('--nside', action="store", type=int, default=100)
+parser.add_argument('--mode', action="store", type=str, default='none')
+parser.add_argument('--time_limit', action="store", type=int, default=60)
+parser.add_argument('--learning_rate', action="store", type=float, default=0.001)
+args = parser.parse_args()
+show = args.show
+nside = args.nside
+if args.mode == 'none':
+    mode = None
+else:
+    mode = args.mode
+time_limit = args.time_limit
+learning_rate = args.learning_rate
+
+n_side = 4
+batch_size = 64
+if mode is None:
+    save_dir = sys.argv[0][:-3]
+else:
+    save_dir = sys.argv[0][:-3]+'_'+mode
+
+if os.path.exists('./'+save_dir+'/sample_z'):
+    sample_z = np.loadtxt('./'+save_dir+'/sample_z')
+else:
+    cs.ch_mkdir('./'+save_dir)
+    sample_z = np.random.normal(size=(n_side**2, 512))
+    np.savetxt('./'+save_dir+'/sample_z',sample_z)
+
+dp = Simple_String(nx=nside,ny=nside,num=100,augment=True,reinit=100,mode=mode)
+
+if show:
+    sims = dp(10)
+    for i in range(10):
+        img = sims[i,:,:,0]
+        plt.imshow(img,origin='lower',cmap='gray')
+        plt.show()
+        plt.close()
+
+checkpoint_dir = './'+save_dir+'/checkpoint'
+sample_dir = './'+save_dir+'/samples'
+log_dir = './'+save_dir+'/logs'
+
+dcgan = cs.DCGAN(
+    data_provider = dp,
+    batch_size=batch_size, 
+    n_side=n_side,sample_z = sample_z,
+    gf_dim=32, df_dim=32,
+    label_real_lower=.9, label_fake_upper=.1,
+    z_dim=512,save_per = 100)
+
+dcgan.train(num_epoch=100000,batch_per_epoch=10,sample_per=1,verbose=1,\
+learning_rate=learning_rate,D_update_per_batch=2,G_update_per_batch=1,\
+sample_dir=sample_dir,checkpoint_dir=checkpoint_dir,\
+log_dir=log_dir,time_limit=time_limit)
+
+cs.movie(sample_dir,output=save_dir+'/movie')
+
+
+
+
+
+
+
+
+
---
+++ scripts/simple_string_example/utils.py
@@ -0,0 +1,124 @@
+import cv2
+import random
+import numpy as np
+from skimage.draw import polygon
+from scipy.ndimage.interpolation import rotate
+
+def canny(d,edege):
+
+    if edege=='lap':
+        d = cv2.Laplacian(d,cv2.CV_64F)
+
+    elif edege=='sob':
+        sobelx = cv2.Sobel(d,cv2.CV_64F,1,0,ksize=3)
+        sobely = cv2.Sobel(d,cv2.CV_64F,0,1,ksize=3)
+        d =np.sqrt(sobelx**2+sobely**2)
+
+    elif edege=='sch':
+        scharrx = cv2.Scharr(d,cv2.CV_64F,1,0)
+        scharry = cv2.Scharr(d,cv2.CV_64F,0,1)
+        d =np.sqrt(scharrx**2+scharry**2)
+        
+    else:
+        assert 0,'Mode have to be lap, sob or sch!' 
+
+    return d
+
+class Simple_String(object):
+
+    def __init__(self,nx=200,ny=200,num=100,augment=True,
+                reinit=0,ns=20,l_min=None,l_max=None,mode=None):
+        self.nx = nx
+        self.ny = ny
+        if augment:
+            num = 8*num
+        self.num = num
+        self.augment = augment
+        self.reinit = reinit
+        self.ns = ns
+        if l_min is None:
+            self.l_min = min(nx,ny)//10
+        else:
+            self.l_min = l_min
+        if l_max is None:
+            self.l_max = min(nx,ny)//3
+        else:
+            self.l_max = l_max
+        if reinit:
+            self.season = 0
+        self.mode = mode
+        self.strings = np.zeros((num,nx,ny,1))
+        self.reinitiate()
+        
+    def augmentation(self,m,i):
+        i_rot = i%4
+        i_flp = i//4
+        if i_rot:
+            m = np.rot90(m,i_rot)
+        if i_flp:
+            m = np.fliplr(m)
+        return m
+    
+    def reinitiate(self):
+        if self.augment:
+            for i in range(0,self.num,8):
+                m = self.string()
+                for j in range(8):
+                    self.strings[i+j,:,:,0] = self.augmentation(m,j)    
+        else:
+            for i in range(self.num):
+                self.strings[i,:,:,0] = self.string()
+        
+    def string(self,ns=None):
+        
+        if ns is None:
+            ns = self.ns
+    
+        imx = self.nx+100
+        imy = self.ny+100
+        
+        rec = np.zeros((imx,imy))
+        for i in range(ns):
+            width = 2*np.random.randint(self.l_min,self.l_max)
+            height = width/2
+            rand = np.random.randint((imx-width)*(imy-height))
+            
+            r0 = rand%(imx-width)
+            c0 = rand//(imx-width)
+            angle = np.random.uniform(0,180)
+        
+            rr, cc = [r0, r0 + width//2, r0 + width//2., r0], [c0, c0, c0 + height, c0 + height]
+            rr, cc = polygon(rr, cc)    
+            rec[rr, cc] += 1
+            
+            rr, cc = [r0 + width//2, r0 + width, r0 + width, r0 + width//2], [c0, c0, c0 + height, c0 + height]
+            rr, cc = polygon(rr, cc)    
+            rec[rr, cc] = -1
+        
+            rec = rotate(rec, angle, axes=(1, 0), reshape=0) 
+            
+        if self.mode is None:
+            return rec[50:-50,50:-50]
+        else:
+            return canny(rec[50:-50,50:-50],self.mode)
+            
+    def __call__(self,n):
+    
+        if self.reinit:
+            if self.season%self.reinit==self.reinit-1:
+                print 'Reinitiation...'
+                self.reinitiate()
+            self.season += 1
+            
+        idx = np.arange(self.num)
+        random.shuffle(idx)
+        idx = idx[:n]
+        return self.strings[idx]
+    
+
+
+
+
+
+
+
